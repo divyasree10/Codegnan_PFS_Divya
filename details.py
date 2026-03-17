f04a6ed908f6855570c37f91a3b78bb67d30dcfa
divyasree10 <divyasree.veludurthi@gmail.com>
2026-03-17 13:25:29 +0530

    day1push

---
+++ details.py
@@ -0,0 +1,9 @@
+print("My name is V. Divya Sree")
+print("__________________________________________________")
+print("I graduated from Vignan's Institute of Engineering for Women")
+print("__________________________________________________")
+print("My stream is Computer Science and Engineering")
+print("__________________________________________________")
+print("Currently I am learning Python Full Stack")
+print("__________________________________________________")
+
